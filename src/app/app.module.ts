fc52474fa0c3a1c6616ae66485d0f89b1bb58066
RamezYoussef712 <ramez.youssef.fahmy@gmail.com>
2022-05-09 12:48:59 +0200

    final

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { StudentComponent } from './Student/student.component';
+import { DepartmentComponent } from './department/department.component';
+import { HeaderComponent } from './header/header.component';
+import { ContentComponent } from './content/content.component';
+import { SideComponent } from './side/side.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent, StudentComponent, DepartmentComponent, HeaderComponent, ContentComponent, SideComponent, FooterComponent
   ],
   imports: [
     BrowserModule
